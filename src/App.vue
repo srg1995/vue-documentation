7d3b4a27d0dc3eedcea1fa364cbf2ef99ca8a114
Sergio Sacristan Santero <srg_ek@hotmail.com>
2025-03-25 14:04:34 +0100

    first component

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,10 @@
-<script setup></script>
+<script setup>
+import FirstComponent from '@/components/FirstComponent.vue'
+</script>
 
 <template>
   <h1>Hola mundo</h1>
+  <FirstComponent />
 </template>
 
 <style scoped></style>
