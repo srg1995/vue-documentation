13c3fdca31fd5586cee2bcd77bf50e493275b240
Sergio Sacristan Santero <srg_ek@hotmail.com>
2025-03-25 14:08:39 +0100

    component logic

--- src/components/FirstComponent.vue
+++ src/components/FirstComponent.vue
@@ -1,7 +1,21 @@
-<script setup></script>
+<script setup>
+import { ref } from 'vue'
+const count = ref(0)
+const handleClick = () => {
+  count.value++
+}
+</script>
 
 <template>
   <h1>Este es mi primer componente</h1>
+  <div>
+    <p>Contador: {{ count }}</p>
+    <button @click="handleClick">Incrementar</button>
+  </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+div {
+  color: white;
+}
+</style>
