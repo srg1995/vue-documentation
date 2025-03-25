26c5fb9ecf0c977266352abb9feb756a5b28b8c0
Sergio Sacristan Santero <srg_ek@hotmail.com>
2025-03-25 19:53:50 +0100

    computed values

--- src/components/FirstComponent.vue
+++ src/components/FirstComponent.vue
@@ -1,15 +1,17 @@
 <script setup>
-import { ref } from 'vue'
+import { computed, ref } from 'vue'
 const count = ref(0)
 const handleClick = () => {
   count.value++
 }
+const doubleCount = computed(() => count.value * 2)
 </script>
 
 <template>
   <h1>Este es mi primer componente</h1>
   <div>
     <p>Contador: {{ count }}</p>
+    <p>Contador doble computado: {{ doubleCount }}</p>
     <button @click="handleClick">Incrementar</button>
   </div>
 </template>
