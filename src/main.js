8e889977186c149423185b4fba92125ef6eb0a03
Sergio Sacristan Santero <srg_ek@hotmail.com>
2025-03-26 13:06:35 +0100

    implements lazyload

--- src/main.js
+++ src/main.js
@@ -4,12 +4,14 @@ import App from './App.vue'
 import router from './router'
 import { createPinia } from 'pinia'
 import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
+import vueLazyLoad from 'vue3-lazyload'
 
 const app = createApp(App)
 
 const pinia = createPinia()
 pinia.use(piniaPluginPersistedstate)
 
+app.use(vueLazyLoad, {})
 app.use(pinia)
 app.use(router)
 
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,12 +1,44 @@
 <script lang="js" setup>
 import MainLayout from '@/layouts/MainLayout.vue'
+import { ref } from 'vue'
+
+const lazyOptions = ref({
+  src: 'https://picsum.photos/1280/800',
+  lifecycle: {
+    loading: (el) => {
+      console.log('cargando imagen', el)
+    },
+    error: (el) => {
+      console.log('error cargando imagen', el)
+    },
+    loaded: (el) => {
+      console.log('imagen cargada', el)
+    },
+  },
+})
 </script>
 
 <template>
   <MainLayout>
     <template v-slot:section>
       <h1 class="text-3xl font-bold underline text-center text-gray-500">Home View</h1>
+      <div class="flex justify-center w-full p-8">
+        <img
+          lazy="loading"
+          v-lazy="{
+            src: lazyOptions.src,
+            lifecycle: lazyOptions.lifecycle,
+            delay: 200,
+          }"
+        />
+      </div>
     </template>
   </MainLayout>
 </template>
 
+<style scoped>
+img[lazy='loading'] {
+  background-color: rgba(0, 0, 255, 0.383);
+  width: 520px;
+}
+</style>
